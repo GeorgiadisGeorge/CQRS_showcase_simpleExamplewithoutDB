04ef5afa468d056cbbd69cba53811d6bcb1b0d25
GeorgiadisGeorge <107368646+GeorgiadisGeorge@users.noreply.github.com>
2023-04-03 16:02:36 +0300

    Added handling

---
+++ CQRS_showcase/CQRS/CandidateHandler.cs
@@ -0,0 +1,42 @@
+﻿using CQRS_showcase.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CQRS_showcase.CQRS
+{
+    public class CandidateHandler
+    {
+        private readonly List<Candidate> _candidates;
+
+        public CandidateHandler(List<Candidate> candidates)
+        {
+            _candidates = candidates;
+        }
+
+        public void Handle(AddCandidateCommand command)
+        {
+            var candidates = new[]
+            {
+                new Candidate { Name = "John Doe", Age = 30, Experience = "5 years" },
+                new Candidate { Name = "George Georgiou", Age = 90, Experience = "50 years" },
+                new Candidate { Name = "Ann Cate", Age = 15, Experience = "10 years" },
+                new Candidate { Name = "Bruce Lee", Age = 150, Experience = "100 years" }
+            };
+
+            foreach (var candidate in candidates)
+            {
+                command.Execute(candidate);
+            }
+        }
+
+
+        public List<Candidate> Handle(GetCandidatesQuery query)
+        {
+            return query.Execute();
+        }
+    }
+
+}
--- CQRS_showcase/Program.cs
+++ CQRS_showcase/Program.cs
@@ -7,21 +7,38 @@ namespace CQRS_showcase
     {
         static void Main(string[] args)
         {
+            //List<Candidate> candidates = new List<Candidate>();
+            //var addCommand = new AddCandidateCommand(candidates);
+            //addCommand.Execute(new Candidate { Name = "John", Age = 25, Experience = "3 years" });
+            //addCommand.Execute(new Candidate { Name = "George", Age = 88, Experience = "80 years" });
+            //addCommand.Execute(new Candidate { Name = "Bruce", Age = 15, Experience = "5 years" });
+
+
+            //var getQuery = new GetCandidatesQuery(candidates);
+            //List<Candidate> candidateList = getQuery.Execute();
+
+            //foreach (var candidate in candidateList)
+            //{
+            //    Console.WriteLine($"Name: {candidate.Name}, Age: {candidate.Age}, Experience: {candidate.Experience}");
+            //}
+
+            // create a list of candidates
             List<Candidate> candidates = new List<Candidate>();
-            var addCommand = new AddCandidateCommand(candidates);
-            addCommand.Execute(new Candidate { Name = "John", Age = 25, Experience = "3 years" });
-            addCommand.Execute(new Candidate { Name = "George", Age = 88, Experience = "80 years" });
-            addCommand.Execute(new Candidate { Name = "Bruce", Age = 15, Experience = "5 years" });
 
+            // create a handler for the candidates
+            CandidateHandler handler = new CandidateHandler(candidates);
 
-            var getQuery = new GetCandidatesQuery(candidates);
-            List<Candidate> candidateList = getQuery.Execute();
+            // add some candidates
+            handler.Handle(new AddCandidateCommand(candidates));
 
-            foreach (var candidate in candidateList)
+            // get all candidates and console write their names
+            List<Candidate> allCandidates = handler.Handle(new GetCandidatesQuery(candidates));
+
+            foreach (Candidate candidate in allCandidates)
             {
-                Console.WriteLine($"Name: {candidate.Name}, Age: {candidate.Age}, Experience: {candidate.Experience}");
+                Console.WriteLine($"Name = {candidate.Name}, Age = {candidate.Age}, Experience = {candidate.Experience}");
             }
-            
+
         }
     }
 }
